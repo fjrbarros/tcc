060600a0e483af86d8143fe9e04139a8c8d9c803
Fabio Barros <fabio.junior643@outlook.com>
2021-02-01 14:11:31 -0300

    Cadastro projeto

--- src/pages/projeto/Cadastro.jsx
+++ src/pages/projeto/Cadastro.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
-import { ContainerRoot, ContainerContent, Form, TextField } from '../../components/Index';
-import { SelectField, DateField } from '../../components/Index';
+import { ContainerRoot, ContainerContent, Form, TextField, SelectField, DateField, EmailField } from '../../components/Index';
 import { DefaultPage } from '../Index';
 import { useSelector } from 'react-redux';
 import { Box, makeStyles, Tooltip } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 
 
 const useStyles = makeStyles(theme => ({
@@ -14,19 +20,26 @@ const useStyles = makeStyles(theme => ({
     },
 
     selectField: {
-        width: '100%'
+        width: '100%',
+        marginTop: '16px',
+        marginBottom: '8px',
     }
 }));
+
+function defaultValues() {
+    return {
+        tipoProjeto: '',
+        dataInicio: new Date(),
+        dataPrevistaTermino: new Date(),
+        membros: [{ id: 1, nome: 'teste' }, { id: 2, nome: 'teste 2' }]
+    }
+}
 
 export default function CadastroProjeto() {
     const classes = useStyles();
     const enums = useSelector(state => state.enums);
     const enumTipoProjeto = enums.enumTipoProjeto;
-    const [values, setValues] = useState({
-        tipoProjeto: '',
-        dataInicio: new Date(),
-        dataPrevistaTermino: new Date()
-    });
+    const [values, setValues] = useState(defaultValues());
 
     function handleChange(event) {
         setValues({ ...values, [event.target.name]: event.target.value });
@@ -62,11 +75,36 @@ export default function CadastroProjeto() {
                                 onChange={date => setValues({ ...values, dataPrevistaTermino: date })}
                             />
                         </Box>
-                        <Tooltip title='Envolvidos no projeto' placement='left'>
-                            <IconButton style={{ float: 'right' }}>
-                                <AddCircleIcon />
-                            </IconButton>
-                        </Tooltip>
+                        <Box display='flex' justifyContent='flex-end'>
+                            <EmailField label='E-mail' style={{ marginRight: '5px' }} />
+                            <SelectField
+                                className={classes.selectField}
+                                name='tipoProjeto'
+                                data={enumTipoProjeto}
+                                label='Tipo projeto'
+                                value={values.tipoProjeto}
+                                onChange={handleChange}
+                            />
+                            <Tooltip title='Envolvidos no projeto' placement='left'>
+                                <IconButton>
+                                    <AddCircleIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                        <List >
+                            {
+                                values.membros.map(item => {
+                                    return (
+                                        <ListItem
+                                            key={item.id}
+                                        >
+                                            <ListItemIcon><BookmarkBorderIcon /></ListItemIcon>
+                                            <ListItemText>{item.nome}</ListItemText>
+                                        </ListItem>
+                                    )
+                                })
+                            }
+                        </List>
                     </Form>
                 </ContainerContent>
             </ContainerRoot>
